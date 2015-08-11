9baaacab439eb7ba5f60205bf14d519b8b212651
MyWayToJavaJunior <hyperjava@mail.ru>
2015-08-11 13:51:34 +0600

    initial commit

---
+++ bytestream/src/main/java/kz/e16training/bytestream/Run.java
@@ -0,0 +1,13 @@
+package kz.e16training.bytestream;
+
+
+/**
+ * @author DK
+ * @version 1.0.0
+ *
+ */
+public class Run {
+    public static void main(String[] args) {
+  
+    }
+}
---
+++ serialization/src/main/java/kz/e16training/serialization/Run.java
@@ -0,0 +1,12 @@
+package kz.e16training.serialization;
+
+/**
+ * @author DK
+ * @version 1.0.0
+ *
+ */
+public class Run {
+    public static void main(String[] args) {
+  
+    }
+}
