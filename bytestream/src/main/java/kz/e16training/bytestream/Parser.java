a0393d9ed9a929b4d232ed602b723492d90bbe8a
MyWayToJavaJunior <hyperjava@mail.ru>
2015-08-13 21:10:29 +0600

    minor changes

---
+++ bytestream/src/main/java/kz/e16training/bytestream/Parser.java
@@ -0,0 +1,14 @@
+package kz.e16training.bytestream;
+
+/**
+ * Created by X1 on 12.08.2015.
+ */
+public class Parser {
+    private String nameOfFile;
+
+    public Parser(String nameOfFile) {
+        this.nameOfFile = nameOfFile;
+    }
+
+    
+}
